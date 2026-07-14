3713a94ac00dec76e48eb687966f240cf8905eca
Henrique C. de Andrade <rique9906@gmail.com>
2026-07-13 21:02:13 -0300

    Create main.cpp

---
+++ Variables-Types-Objects/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int x = 12;
+
+    int a, b; 
+
+    // TENTE RODAR ESSAS LINHAS DE CODIGO:
+    // int a, int b;
+    // int a, double b;
+    
+    int a;
+    double b;
+
+    return 0;
+}
